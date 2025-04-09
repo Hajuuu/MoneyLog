adc5e8f9b11a24f7f548534c333b2431ed9e3ece
Hajuuu <hpt0127@naver.com>
2025-04-09 11:49:03 +0900

    fix: 폴더 구조 변경 및 main.js 변경
    moneyLog 폴더 삭제 및 fontawesome import 삭제

--- src/main.js
+++ src/main.js
@@ -7,11 +7,7 @@ import App from './App.vue';
 import router from './router';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 const app = createApp(App);
-
-app.component('fontAwesomeIcon', FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);

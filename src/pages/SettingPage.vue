92163cbee1ac8d068620e8e966d629d935f70aff
hyun <nemo247300@gmail.com>
2025-04-09 16:10:56 +0900

    Feat : 설정창 binding 값 주석처리

--- src/pages/SettingPage.vue
+++ src/pages/SettingPage.vue
@@ -10,7 +10,7 @@
     <!-- 프로필 이미지 -->
     <div class="">
       <div class="">
-        <img :src="defaultAvatar" class="" style="width: 100px; height: 100px" alt="" />
+        <!-- <img :src="defaultAvatar" class="" style="width: 100px; height: 100px" alt="" /> -->
         <span class="" style="font-size: 0.9rem">
           <i class="fas fa-pen"></i>
         </span>
@@ -20,11 +20,11 @@
     <div class="">
       <div class="">
         <span class="">이름</span>
-        <span>{{ user.name }}</span>
+        <!-- <span>{{ user.name }}</span> -->
       </div>
       <div class="">
         <span class="">이메일 주소</span>
-        <span>{{ user.email }}</span>
+        <!-- <span>{{ user.email }}</span> -->
       </div>
     </div>
   </div>

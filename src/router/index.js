37aa51ffe67e06511679f0e3b8b40ad2bdeb0a90
hyun <nemo247300@gmail.com>
2025-04-09 15:59:23 +0900

    Feat : 라우터 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import TransactionsPage from '../pages/TransactionsPage.vue';
+import StatisticsPage from '../pages/StatisticsPage.vue';
+import GoalPage from '../pages/GoalPage.vue';
+import SettingPage from '../pages/SettingPage.vue';
+import SettingImgPage from '../pages/SettingImgPage.vue';
+import SettingProfilePage from '../pages/SettingProfilePage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +19,31 @@ const router = createRouter({
       path: '/transactions',
       name: 'transactions',
       component: TransactionsPage,
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: StatisticsPage,
+    },
+    {
+      path: '/goal',
+      name: 'goal',
+      component: GoalPage,
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: SettingPage,
+    },
+    {
+      path: '/setting/img',
+      name: 'settingimg',
+      component: SettingImgPage,
+    },
+    {
+      path: '/setting/profile',
+      name: 'settingprofile',
+      component: SettingProfilePage,
     },
   ],
 });
